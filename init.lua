90e2cf0a3d4bac42916884a62e6c2249f4ba5bc2
dave caruso <me@paperdave.net>
2024-06-03 23:25:40 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,108 @@
+require("dave.remap")
+require("dave.set")
+vim.g.codeium_disable_bindings = 1;
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable" })
+end
+vim.opt.rtp:prepend(lazypath)
+-- dependencies
+require("lazy").setup({
+	{'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+	config = function () 
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = { 
+				"c", "lua", "vim", "vimdoc", "query", 
+				"javascript", "html", "rust", "zig",
+				"typescript"
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },  
+		})
+	end
+	},
+	{ "theprimeagen/harpoon", name = "harpoon" },
+	{ "mbbill/undotree", name = "undotree" },
+	{ "tpope/vim-fugitive", name = "fugitive" },
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+    {'supermaven-inc/supermaven-nvim'},
+});
+
+local lsp = require('lsp-zero')
+lsp.preset("recommended");
+lsp.setup();
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) configure lua language server
+local lua_opts = lsp.nvim_lua_ls()
+require('lspconfig').lua_ls.setup(lua_opts)
+
+-- Replace the language servers listed here
+-- with the ones you have installed
+lsp.setup_servers({'tsserver', 'rust_analyzer', "zls" })
+
+---
+-- Autocompletion config
+---
+local cmp = require('cmp')
+local cmp_action = lsp.cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<Tab>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
+
+require("supermaven-nvim").setup({
+  keymaps = {
+    accept_suggestion = "<C-g>",
+    clear_suggestion = "<C-]>",
+    accept_word = "<C-j>",
+  },
+  ignore_filetypes = { },
+  color = {
+    -- suggestion_color = "#ffffff",
+    -- cterm = 244,
+  },
+  disable_inline_completion = false, -- disables inline completion for use with cmp
+  disable_keymaps = false -- disables built in keymaps for more manual control
+})
+
+require("catppuccin").setup({
+})
+
+vim.cmd.colorscheme "catppuccin"
+
